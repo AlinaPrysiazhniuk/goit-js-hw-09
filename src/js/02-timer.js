c476132850848d4a5711f1e6f1d92dd225a0c6d0
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-03-20 17:24:21 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -28,32 +28,32 @@ const options = {
           const deltaTime = selectedDates[0] - new Date();
           const { days, hours, minutes, seconds } = convertMs(deltaTime);
 
-          //timer({ days, hours, minutes, seconds });
-          refs.dataDays.innerHTML = days < 10 ? addLeadingZero(days) : days;
-          refs.dataHours.innerHTML = hours < 10 ? addLeadingZero(hours) : hours;
-          refs.dataMinutes.innerHTML =
-            minutes < 10 ? addLeadingZero(minutes) : minutes;
-          refs.dataSeconds.innerHTML =
-            seconds < 10 ? addLeadingZero(seconds) : seconds;
-          if (days === 00 && hours === 00 && minutes === 00 && seconds === 00) {
-            clearInterval(timerActive);
-          }
+          timer({ days, hours, minutes, seconds });
+          // refs.dataDays.innerHTML = days < 10 ? addLeadingZero(days) : days;
+          // refs.dataHours.innerHTML = hours < 10 ? addLeadingZero(hours) : hours;
+          // refs.dataMinutes.innerHTML =
+          //   minutes < 10 ? addLeadingZero(minutes) : minutes;
+          // refs.dataSeconds.innerHTML =
+          //   seconds < 10 ? addLeadingZero(seconds) : seconds;
+          // if (days === 00 && hours === 00 && minutes === 00 && seconds === 00) {
+          //   clearInterval(timerActive);
+          //}
         }, 1000);
       });
     }
   },
 };
 
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
-
-// function timer({ days, hours, minutes, seconds }) {
-//   refs.dataDays.textContent = `${days}`;
-//   refs.dataHours.textContent = `${hours}`;
-//   refs.dataMinutes.textContent = `${minutes}`;
-//   refs.dataSeconds.textContent = `${seconds}`;
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
 // }
+
+function timer({ days, hours, minutes, seconds }) {
+  refs.dataDays.textContent = `${days}`;
+  refs.dataHours.textContent = `${hours}`;
+  refs.dataMinutes.textContent = `${minutes}`;
+  refs.dataSeconds.textContent = `${seconds}`;
+}
 
 flatpickr('#datetime-picker', options);
 function convertMs(ms) {
